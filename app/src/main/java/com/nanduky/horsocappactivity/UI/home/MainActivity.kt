b7c85f8fb6ccf6ef05cc499c8b5d811eea6f47b3
Nanduky <nanduky2@gmail.com>
2024-10-11 22:50:39 +0200

    Carga inicial del proyecto Android

---
+++ app/src/main/java/com/nanduky/horsocappactivity/UI/home/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.nanduky.horsocappactivity.UI.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.nanduky.horsocappactivity.R
+import com.nanduky.horsocappactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigator()
+    }
+
+    private fun initNavigator() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHost.navController
+        binding.btnNavView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/nanduky/horsocappactivity/UI/luck/LackFragment.kt
@@ -0,0 +1,23 @@
+package com.nanduky.horsocappactivity.UI.luck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nanduky.horsocappactivity.databinding.FragmentLackBinding
+
+class LackFragment : Fragment() {
+
+    private var _binding: FragmentLackBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentLackBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
